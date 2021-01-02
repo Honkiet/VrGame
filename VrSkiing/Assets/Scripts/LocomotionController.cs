555e36f6c0594cde0a0a190292f48dedf5df82ea
Hendrik Truong <truheco@gmail.com>
2021-01-02 22:05:03 +0100

    Started the menu
    Added Shadder for line
    Added Canvas with Interaction
    
    Fix Ray Interacting with Canvas.

---
+++ VrSkiing/Assets/Scripts/LocomotionController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LocomotionController : MonoBehaviour
+{
+    public XRController leftRay;
+    public XRController rightRay;
+    public InputHelpers.Button UiActivationButton;
+    public float activationThreshold = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leftRay)
+        {
+            leftRay.gameObject.SetActive(CheckIfActivated(leftRay));
+        }
+        if (rightRay)
+        {
+            rightRay.gameObject.SetActive(CheckIfActivated(rightRay));
+        }
+    }
+
+    public bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, UiActivationButton, out bool isActivated, activationThreshold);
+        return isActivated;
+    }
+}
