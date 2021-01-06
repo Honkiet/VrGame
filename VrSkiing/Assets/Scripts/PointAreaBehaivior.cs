35e88e7648e631a9ab3911f2617e43e8bd994a38
Hendrik Truong <truheco@gmail.com>
2021-01-06 17:21:49 +0100

    Added Point Area Minigame
    Started working on the point area minigame.
    Added new shader
    Added PoinAreaBehaivior script
    - Uses Moves towards target x value.
    ToDo :
    Add Random new Target that is valid
    Add Shrinking
    Add Point system

---
+++ VrSkiing/Assets/Scripts/PointAreaBehaivior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAreaBehaivior : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+    [SerializeField] Transform area;
+    [SerializeField] Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveTowardsTarget(target);
+    }
+
+    void MoveTowardsTarget(Transform target)
+    {
+
+        var offset = target.position.x - transform.position.x; // Is transform.position rght ?
+        //Get the difference.
+        if (offset > .1f) // as long as there is a diference greater than .1f do this
+        {
+            offset = 1 * speed;
+
+            area.position += new Vector3(offset * Time.deltaTime,0,0) ;
+
+        }
+
+        else if (offset < -0.1f) // if x is negative
+        {
+            offset = -1 * speed;
+
+            area.position += new Vector3(offset * Time.deltaTime, 0, 0);
+
+        }
+    }
+}
