fbf21e0c00dc85037c84b8cfd8fc42855301ccb7
Hendrik Truong <truheco@gmail.com>
2021-01-07 11:01:42 +0100

    Brought File structure into the Project
    Started dividing scripts/prefabs into folders to make it easier to navigate for team work.
    
    Designer can access "Game" folder for prefabs, Materials, Shaders etc.
    
    Programmer has his "Script" folder etc.

--- VrSkiing/Assets/Scripts/PointAreaMinigame/PointAreaBehaivior.cs
+++ VrSkiing/Assets/Scripts/PointAreaMinigame/PointAreaBehaivior.cs
@@ -51,4 +51,6 @@ public class PointAreaBehaivior : MonoBehaviour
     {
        // GetComponent<Transform>().localScale 
     }
+
+
 }
---
+++ VrSkiing/Assets/Scripts/PointAreaMinigame/PointController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PointController : MonoBehaviour
+{
+   
+    public int damage;
+    public TextMeshProUGUI score;
+    public TextMeshProUGUI time;
+    public float timeRemaining = 10;
+    private bool clicked = false;
+
+    public void AddScore(int x)
+    {
+        damage = damage + x;
+        score.text = "Damage : $" + damage.ToString();
+    }
+}
